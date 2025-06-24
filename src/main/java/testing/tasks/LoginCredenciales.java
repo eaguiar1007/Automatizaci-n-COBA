a2bb28c7f8e167377142a8125f6dfe285161b371
eaguiar1007 <eaguiar@bancodebogota.com.co>
2025-06-24 18:10:45 -0500

    se agregan los features

---
+++ src/main/java/testing/tasks/LoginCredenciales.java
@@ -0,0 +1,33 @@
+package testing.tasks;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+import testing.ui.LoginPage;
+import java.util.concurrent.TimeUnit;
+
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+import static testing.ui.LoginUI.TXT_USUARIO;
+
+public class LoginCredenciales implements Task {
+
+    public static Login conCredenciales() {
+        return instrumented(Login.class);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                //Enter.theValue("iparada").into(TXT_USUARIO)
+        );
+
+        try {
+            Thread.sleep(3000); // espera 3 segundos
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
--- src/test/java/testing/runners/CucumberTestSuiteTest.java
+++ src/test/java/testing/runners/CucumberTestSuiteTest.java
@@ -8,9 +8,10 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty", "html:target/cucumber-reports/cucumber.html"},
-        features = "src/test/resources/features/search/LoginPage.feature",
+        features = "src/test/resources/features/search/Login.feature",
         glue = "testing/stepdefinitions",
-        snippets = CucumberOptions.SnippetType.CAMELCASE
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        tags = "@LoginExitoso"
 )
 public class CucumberTestSuiteTest {}
 
