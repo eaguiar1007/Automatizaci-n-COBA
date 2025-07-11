de5abcb429afda193c41d2093eb9a22a0da06066
eaguiar1007 <eaguiar@bancodebogota.com.co>
2025-07-11 16:02:30 -0500

    solucion shadow

--- src/main/java/testing/ui/LoginUI.java
+++ src/main/java/testing/ui/LoginUI.java
@@ -2,19 +2,27 @@ package testing.ui;
 
 import net.serenitybdd.screenplay.targets.Target;
 import net.thucydides.core.webdriver.shadow.ByShadow;
-import org.openqa.selenium.By;
 
 public class LoginUI {
 
-
-
-
-    //cssSelector, shadowHost1, shadowHost2)
     public static final Target TXT_USUARIO = Target.the("campo usuario")
             .located(ByShadow.cssSelector(
-                    "input#user", // Selector dentro del último Shadow DOM
-                    "sp-tp-xcenter-login-form", // Shadow Host 1 (más externo)
-                    "sp-ml-xcenter-login-form", // Shadow Host 2
-                    "sp-at-input" // Shadow Host 3 (más interno)
+                "input[placeholder='Ingresa el usuario']",        // Selector interno final
+                "sp-ml-xcenter-login-form",                             // Shadow host nivel 1
+                    "sp-at-input[id-el='user']"         // Shadow host nivel 2
+            ));
+
+    public static final Target TXT_CLAVE = Target.the("campo clave")
+            .located(ByShadow.cssSelector(
+                    "input[placeholder='Ingresa la contraseña']",
+                    "sp-ml-xcenter-login-form",
+                    "sp-at-input[id-el='password']"
+            ));
+
+    public static final Target BTN_INGRESAR = Target.the("boton ingresar")
+            .located(ByShadow.cssSelector(
+                    "input[placeholder='Ingresa la contraseña']",
+                    "sp-ml-xcenter-login-form",
+                    "sp-at-input[id-el='password']"
             ));
 }
